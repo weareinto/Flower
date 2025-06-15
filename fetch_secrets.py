c90ee4fbb1b57816ca8672d640f17e28e8680cd0
AMIN MAC <aminmac@AMINs-MacBook-Pro.local>
2025-06-15 20:50:20 +0100

    update cicd

--- fetch_secrets.py
+++ fetch_secrets.py
@@ -1,33 +1,3 @@
-# import os
-# from google.cloud import secretmanager
-
-# def access_secret_version(project_id, secret_id, version_id="latest"):
-#     client = secretmanager.SecretManagerServiceClient()
-#     name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-#     response = client.access_secret_version(request={"name": name})
-#     return response.payload.data.decode("UTF-8")
-
-# def load_secrets_to_env(secret_data):
-#     lines = secret_data.split('\n')
-#     for line in lines:
-#         if line.strip():  # Ignore empty lines
-#             key, value = line.split('=', 1)
-#             os.environ[key] = value
-#             print(key, value)
-
-# if __name__ == "__main__":
-    
-#     # project_id = "PROJECT_ID"
-#     project_id = "PROJECT_ID"
-#     secret_id = "staging_env_vars"
-    
-#     # Fetch the secret from Secret Manager
-#     secret_data = access_secret_version(project_id, secret_id)
-    
-#     # Load secrets into environment variables
-#     load_secrets_to_env(secret_data)
-
-
 import os
 from google.cloud import secretmanager
 
@@ -49,7 +19,7 @@ def save_secrets_to_env_file(secret_data, file_path="/app/.env"):
 
 if __name__ == "__main__":
     project_id = "PROJECT_ID"
-    secret_id = "api_workers_secrets"
+    secret_id = "SECRET_ID"
     
     # Fetch the secret from Secret Manager
     secret_data = access_secret_version(project_id, secret_id)
